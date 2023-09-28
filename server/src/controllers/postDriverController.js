24daadb6fe296bca0ae92d01c376a48dde537e75
Evelyn Pereira <eve.cari.perey@gmail.com>
2023-09-28 00:48:38 -0300

    changes get and post

--- server/src/controllers/postDriverController.js
+++ server/src/controllers/postDriverController.js
@@ -20,3 +20,25 @@ const createDriver = async (
   });
 };
 module.exports = { createDriver };
+
+// const {Dogs, Temperaments, dogsTemperaments} = require('../db')
+
+// const createDogDB = async (image, name, height, weight, life_span, temperaments) => {
+
+//     const getTemps = await Temperaments.findAll({where: {name: temperaments, attributes: id} })
+
+//     const resultados = getTemps.map((temp) => ({
+//         id: temp.id,
+//     }))
+
+//     const newDog = await Dogs.create({
+//         image: image,
+//         name: name,
+//         height: height,
+//         weight: weight,
+//         life_span: life_span,
+//         temperaments: resultados,
+//     });
+
+//     return newDog;
+// }
